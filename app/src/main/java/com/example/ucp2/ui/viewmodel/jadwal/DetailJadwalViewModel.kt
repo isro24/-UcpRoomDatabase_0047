307ffabe66216cb9b851c46b57c0e158a2d68f6a
isro24 <alamat_email_anda@example.com>
2024-12-22 17:57:12 +0700

    Menambah DetailJadwalViewModel pada viewmodel

---
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/jadwal/DetailJadwalViewModel.kt
@@ -0,0 +1,84 @@
+package com.example.ucp2.ui.viewmodel.jadwal
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2.data.entity.Jadwal
+import com.example.ucp2.repository.RepositoryJadwal
+import com.example.ucp2.ui.navigation.AlamatNavigasi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+class DetailJadwalViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryJadwal: RepositoryJadwal,
+) : ViewModel(){
+    private val _id: String = checkNotNull(savedStateHandle[AlamatNavigasi.DestinasiDetailJadwal.ID])
+
+    val detailUiState: StateFlow<DetailUiState> = repositoryJadwal.getJdwl(_id)
+        .filterNotNull()
+        .map {
+            DetailUiState(
+                detailUiEvent = it.toDetailUiEvent(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(DetailUiState(isLoading = true))
+            delay(600)
+        }
+        .catch {
+            emit(
+                DetailUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi kesalahan",
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DetailUiState(
+                isLoading = true
+            ),
+        )
+    fun deleteJdwl(){
+        val currentEvent = detailUiState.value.detailUiEvent
+        if (currentEvent != JadwalEvent()){
+            viewModelScope.launch {
+                repositoryJadwal.deleteJdwl(currentEvent.toJadwalEntity())
+            }
+        }
+    }
+}
+
+data class DetailUiState(
+    val detailUiEvent: JadwalEvent = JadwalEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = "",
+){
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == JadwalEvent()
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent !=  JadwalEvent()
+}
+
+fun Jadwal.toDetailUiEvent(): JadwalEvent {
+    return JadwalEvent(
+        id = id,
+        namaDokter = namaDokter,
+        namaPasien = namaPasien,
+        noHp = noHp,
+        tanggalKonsultasi = tanggalKonsultasi,
+        status = status
+    )
+}
