8eddf230a3377243f666067890f1524cd8f4912f
isro24 <alamat_email_anda@example.com>
2024-12-22 19:36:36 +0700

    Menambah initializer UpdateJadwalViewModel pada PenyediaViewModel

--- app/src/main/java/com/example/ucp2/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/PenyediaViewModel.kt
@@ -40,6 +40,12 @@ object PenyediaViewModel {
                 klinikApp().containerApp.repositoryJadwal
             )
         }
+        initializer {
+            UpdateJadwalViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryJadwal
+            )
+        }
     }
 }
 
