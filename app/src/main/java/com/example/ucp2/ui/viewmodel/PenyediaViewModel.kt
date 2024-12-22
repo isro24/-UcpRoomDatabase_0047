09fbc37c0ad8f33eb28a302d94e7e272d96f414f
isro24 <alamat_email_anda@example.com>
2024-12-22 11:10:22 +0700

    Menambah PenyediaViewModel untuk DokterViewModel

---
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.ucp2.ui.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.ucp2.KlinikApp
+import com.example.ucp2.ui.viewmodel.dokter.DokterViewModel
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            DokterViewModel(
+                klinikApp().containerApp.repositoryDokter
+            )
+        }
+    }
+}
+
+fun CreationExtras.klinikApp(): KlinikApp =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KlinikApp)
+
