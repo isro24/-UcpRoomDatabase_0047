bb3ad258abfef1297458b6c6b96720795a9c1b46
isro24 <alamat_email_anda@example.com>
2024-12-23 11:08:34 +0700

    Menambah DynamicSelectedFieldDokter untuk dropdown NamaDokter

--- app/src/main/java/com/example/ucp2/ui/view/jadwal/InsertJadwalView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/jadwal/InsertJadwalView.kt
@@ -23,6 +23,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2.data.NamaDokter
+import com.example.ucp2.ui.costumwidget.DynamicSelectedFieldDokter
 import com.example.ucp2.ui.costumwidget.TopAppBar
 import com.example.ucp2.ui.navigation.AlamatNavigasi
 import com.example.ucp2.ui.viewmodel.jadwal.JadwalEvent
